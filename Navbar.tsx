d1ee169ea45d161e099b3310bc5f74e07ac7da06
yashsehgal <yashsehgal.work@gmail.com>
2022-04-04 13:59:56 +0530

    feat: parent-children block wrapper created

---
+++ Navbar.tsx
@@ -0,0 +1,4 @@
+
+function Navbar(navbarConfiguration) {
+    return navbarConfiguration && null;
+}
--- app.ts
+++ app.ts
@@ -13,6 +13,8 @@ const logWarning = console.warn;
 
 function generateDocumentTree(configurableDocumentObject) {
     
+    let childrenTreeWrapper = [];
+    
     if (configurableDocumentObject === {} || configurableDocumentObject === null || configurableDocumentObject === undefined) {
         configurableDocumentObject = { id: 'parent', contains: null, children: null };
     }
@@ -25,18 +27,38 @@ function generateDocumentTree(configurableDocumentObject) {
     logInfo(chalk.blue('Valid Object: [id: ' + (configurableDocumentObject.id ? configurableDocumentObject.id : null) + '] ====> ' + configurableDocumentObject));
     logInfo(configurableDocumentObject);
 
-    // generating tree structure
+    // generating tree structure for the parent node --- PARENT
     logInfo('PARENT BLOCK');
     logInfo(`${chalk.blueBright('parent')}:id(${configurableDocumentObject.id})`);
     logInfo(`${chalk.blueBright('parent')}:class(${(configurableDocumentObject.className) ? configurableDocumentObject.className : null})`);
     logInfo(`${chalk.blueBright('parent')}:type(${(configurableDocumentObject.type) ? configurableDocumentObject.type : null})`);
-    logInfo(`${chalk.blueBright('parent')}:children(${(configurableDocumentObject.children) ? configurableDocumentObject.children : null})`);
+    logInfo(`${chalk.blueBright('parent')}:children(${(configurableDocumentObject.children) ? configurableDocumentObject.children : null})`);    
+
+    let parentBlockWrapper = {
+        parentElementID: configurableDocumentObject.id,
+        parentElementClassName: configurableDocumentObject.className,
+        parentElementType: configurableDocumentObject.type,
+        parentElementChildren: configurableDocumentObject.children
+    };
+
+    // rendering using DOM methods ---> parentBlockWrapper 
 
     (configurableDocumentObject.children ? generateChildrenTree(configurableDocumentObject.children, (configurableDocumentObject.id ? configurableDocumentObject.id : "id-none")) : generateChildrenTree(null, 'id-none'));
 
     function generateChildrenTree(childrenObject, parentID) {
         if (!childrenObject) return;
-        logInfo(chalk.bgWhite(chalk.black(`CHILDREN OF PARENT[${parentID}]`)));
+
+        let _childrenBlockWrapper = {
+            childrenObject_ParentElement_ID: parentID ? parentID : null,
+            childrenElementID: childrenObject.id ? childrenObject.id : null,
+            childrenElementClassName: childrenObject.className ? childrenObject.className : null,
+            childrenElementType: childrenObject.type ? childrenObject.type : null,
+            childrenElementChildren: (childrenObject.children && childrenObject.children.length > 0) ? childrenObject.children : null
+        };
+
+        childrenTreeWrapper.push(_childrenBlockWrapper);
+
+        logInfo(chalk.bgWhite(chalk.black(`CHILDREN OF [${parentID}]`)));
         logInfo(`${chalk.blueBright('children')}:id(${childrenObject.id})`);
         logInfo(`${chalk.blueBright('children')}:class(${(childrenObject.className) ? childrenObject.className : null})`);
         logInfo(`${chalk.blueBright('children')}:type(${(childrenObject.type) ? childrenObject.type : null})`);
